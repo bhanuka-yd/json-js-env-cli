bd9631f58ddb05d92bafb43a0ac41458c0870356
bhanukayd <bhanuka.yd@gmail.com>
2022-08-30 19:20:00 +0530

    adding

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # CLI for json-js-env
+
+## Usage
+
+```bash
+Usage: json-js-env-cli [options]
+
+Options:
+  -f, --file <file>        JSON or JS env file (multiple files can be provided, duplicate keys will be overwritten) (default: [])
+  --replace-existing-envs  Replace existing environment variables
+  --preserve-attributes    Preserve variable attributes (Will be injected as JSON objects)
+  -c, --command <command>  command to run
+  -h, --help               display help for command
+```
--- bin/json-js.js
+++ bin/json-js.js
@@ -1,22 +1,43 @@
 #!/usr/bin/env node
 const { program } = require('commander');
 const spawn = require('cross-spawn');
+const jsonJS = require("json-js-env");
+const path = require("path");
 
 function collect(value, previous) {
     return previous.concat([value]);
 }
 
 function run(name, options, command) {
-    spawn(program.opts().command, {
-        env: {}
+    const finalEnvs = process.env;
+    const files = program.opts().file;
+    for (var x = 0; x < files.length; x++) {
+        var _path = files[x];
+        if (!path.isAbsolute(_path)) {
+            _path = path.join(process.cwd(), _path);
+        }
+
+        const out = jsonJS({
+            file: _path,
+            preserveAttributes: program.opts().preserveAttributes,
+            replaceExistingENVs: program.opts().replaceExistingEnvs
+        });
+        Object.entries(out).forEach(([k, v]) => finalEnvs[k] = v);
+    }
+    const cmd = program.opts().command.split(/\s+/);
+    const cmd_1 = cmd.splice(0, 1)[0];
+
+    spawn.spawn(cmd_1, cmd, {
+        env: finalEnvs,
+        stdio: "inherit",
     })
 }
 
 program
     .requiredOption('-f, --file <file>', "JSON or JS env file (multiple files can be provided, duplicate keys will be overwritten)", collect, [])
-    .option('--replace-existing', "Replace existing environment variables")
+    .option('--replace-existing-envs', "Replace existing environment variables")
     .option('--preserve-attributes', "Preserve variable attributes (Will be injected as JSON objects)")
-    .requiredOption('-c, --command', "command to run")
+    .requiredOption('-c, --command <command>', "command to run")
     .action(run)
 
 
