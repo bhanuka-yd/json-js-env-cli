96bd8a3a8f35d0f07fae7bc28fa6c5dfa307e424
bhanukayd <bhanuka.yd@gmail.com>
2022-08-30 16:54:51 +0530

    adding

--- bin/json-js.js
+++ bin/json-js.js
@@ -1,24 +1,32 @@
 #!/usr/bin/env node
 const { program } = require('commander');
+const spawn = require('cross-spawn');
 
 function collect(value, previous) {
     return previous.concat([value]);
 }
 
+function run(name, options, command) {
+    spawn(program.opts().command, {
+        env: {}
+    })
+}
+
 program
-    .option('-f, --file <file>', "JSON or JS env file (multiple files can be provided, duplicate keys will be overwritten)", collect, [])
+    .requiredOption('-f, --file <file>', "JSON or JS env file (multiple files can be provided, duplicate keys will be overwritten)", collect, [])
     .option('--replace-existing', "Replace existing environment variables")
     .option('--preserve-attributes', "Preserve variable attributes (Will be injected as JSON objects)")
+    .requiredOption('-c, --command', "command to run")
+    .action(run)
+
 
 program.parse();
 
-function main() {
-    console.log("Hello from main");
-    console.log(program.opts().file)
-    console.log(program.opts().preserveExisting)
-    console.log(program.opts().fileList)
-}
+// function main() {
+//     console.log("Hello from main");
+//     console.log(program.opts())
+// }
 
 // if (require.main === module) {
-main();
+// main();
 // }
